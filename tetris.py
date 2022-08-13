20b8e3a5f7c128c7ce42c003b8b58bcc2d3b6379
Rutzno <d.rutzno@gmail.com>
2022-08-13 02:27:57 +0000

    stage 3

--- tetris.py
+++ tetris.py
@@ -2,7 +2,7 @@ import numpy as np
 """
 __author__ = "Mack_TB"
 __since__ = "11/8/2021"
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 """
 
 
@@ -27,31 +27,48 @@ class Board:
         self.width = width
         self.height = height
         self.grid = np.full((height, width), None)
-        self.current_piece = []
+        self.current_piece = []  # like 'I': [[4, 14, 24, 34], [3, 4, 5, 6]]
         self.current_piece_index = 0
 
     def rotate(self):
-        self.current_piece_index += 1
-        if self.current_piece_index == len(self.current_piece):
-            self.current_piece_index = 0
-        item = self.current_piece[self.current_piece_index]
-        self.put(item, display=False)
-        self.go_down()
+        if self.is_piece_onfloor():
+            # print("floor")
+            self.print_grid()
+        else:
+            self.current_piece_index += 1
+            if self.current_piece_index == len(self.current_piece):
+                self.current_piece_index = 0  # reset: go back to the first index
+            item = self.current_piece[self.current_piece_index]
+            self.put(item, display=False)
+            self.go_down()
 
     def go_down(self):
         self.move(10)
 
     def to_left(self):
-        self.move(9)
+        if any(self.check_left_border()):
+            # print("border_left")
+            self.go_down()
+        else:
+            self.move(9)
 
     def to_right(self):
-        self.move(11)
+        if any(self.check_right_border()):
+            # print("border_right")
+            self.go_down()
+        else:
+            self.move(11)
 
+    # shift all the element of a piece i.e. the unique piece and its rotated pieces
     def move(self, step):
-        for i in range(len(self.current_piece)):
-            for j in range(len(self.current_piece[i])):
-                self.current_piece[i][j] += step
-        self.put(self.current_piece[self.current_piece_index])
+        if self.is_piece_onfloor():
+            # print("floor")
+            self.print_grid()
+        else:
+            for i in range(len(self.current_piece)):
+                for j in range(len(self.current_piece[i])):
+                    self.current_piece[i][j] += step
+            self.put(self.current_piece[self.current_piece_index])
 
     def put(self, item, display=True):
         index = 0
@@ -73,9 +90,10 @@ class Board:
         for row in range(self.height):
             for col in range(self.width):
                 if self.grid[row][col] is None:
-                    print("-", end=" ")
+                    print("-", end="") if col == self.width-1 else print("-", end=" ")
                 else:
-                    print(self.grid[row][col], end=" ")
+                    print(self.grid[row][col], end="") if col == self.width-1 else print(self.grid[row][col], end=" ")
+                    # print(self.grid[row][col], end=" ")
             print()
 
     def reset_grid(self):
@@ -83,3 +101,16 @@ class Board:
             for col in range(self.width):
                 if self.grid[row][col] is not None:
                     self.grid[row][col] = None
+
+    def check_left_border(self):
+        return [True if n % 10 == 0 else False
+                for n in self.current_piece[self.current_piece_index]]
+
+    def check_right_border(self):
+        return [True if n % 10 == 9 else False
+                for n in self.current_piece[self.current_piece_index]]
+
+    def is_piece_onfloor(self):
+        return any(True if n // 10 == self.height-1 else False
+                   for n in self.current_piece[self.current_piece_index])
+
